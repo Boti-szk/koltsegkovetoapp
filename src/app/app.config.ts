6f69dc955f438a4dd36d10f5be7c347b502d9806
SzitasBotond <szitas.botond3@gmail.com>
2025-11-02 20:19:37 +0100

    PW2_2.mk.2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig, isDevMode } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+            provideZoneChangeDetection({eventCoalescing: true}),
             provideRouter(routes),
             provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
